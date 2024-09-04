b02d01f8b942a2eb3008fea5e15af479e6bb4bb1
wuling <wuling@ke.com>
2024-09-04 15:05:18 +0800

    fix: elapsed time sql

--- api/sql/update.20240905.sql
+++ api/sql/update.20240905.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `workflow_run` ADD COLUMN `metadata` varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' AFTER `response_mode`;
 
 ALTER TABLE `workflow_run` ADD COLUMN `thread_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'thread_id' AFTER `metadata`;
+
+ALTER TABLE `workflow_run` ADD COLUMN `elapsed_time` bigint NOT NULL DEFAULT '0' COMMENT '运行耗时' AFTER `thread_id`;

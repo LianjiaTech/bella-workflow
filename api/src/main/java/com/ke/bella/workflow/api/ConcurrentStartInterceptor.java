3bed623c2193935781379cc305151769650f6e6e
chenjiakai004 <chenjiakai004@ke.com>
2024-10-31 22:24:31 +0800

    bugfix: clear context for async started

--- api/src/main/java/com/ke/bella/workflow/api/ConcurrentStartInterceptor.java
+++ api/src/main/java/com/ke/bella/workflow/api/ConcurrentStartInterceptor.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.ke.bella.workflow.db.BellaContext;
+
 @Component
 public class ConcurrentStartInterceptor extends HandlerInterceptorAdapter {
     public static final String ASYNC_REQUEST_MARKER = "ASYNC_REQUEST_MARKER";
@@ -13,5 +15,6 @@ public class ConcurrentStartInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         request.setAttribute(ASYNC_REQUEST_MARKER, Boolean.TRUE);
+        BellaContext.clearAll();
     }
 }

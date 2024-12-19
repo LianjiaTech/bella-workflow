0161917a050dd7de37d02c24fda85eba0c19e6b1
wuling <wuling@ke.com>
2024-12-19 12:12:35 +0800

    fix: datasets tags

--- web/app/(commonLayout)/datasources/DatasetCard.tsx
+++ web/app/(commonLayout)/datasources/DatasetCard.tsx
@@ -34,7 +34,7 @@ const DatasetCard = ({
   const { t } = useTranslation()
   const { notify } = useContext(ToastContext)
   const { isCurrentWorkspaceDatasetOperator } = useAppContext()
-  const [tags, setTags] = useState<Tag[]>(dataset.tags)
+  const [tags, setTags] = useState<Tag[]>(dataset.tags || [])
 
   const [showRenameModal, setShowRenameModal] = useState(false)
   const [showConfirmDelete, setShowConfirmDelete] = useState(false)

5a02c02c997c05d223ff9cf7dfab96423e6defb5
chenjiakai004 <chenjiakai004@ke.com>
2025-05-13 18:22:50 +0800

    clean & fix sql file

--- api/sql/update.20240829.sql
+++ api/sql/update.20240829.sql
@@ -1,7 +1,5 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE `wecom_group_member` ADD COLUMN `robot_outer_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '机器人外部id' AFTER `robot_id`;
-
 ALTER TABLE `workflow_kafka_trigger` ADD COLUMN `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' AFTER `trigger_id`;
 
 ALTER TABLE `workflow_kafka_trigger` ADD COLUMN `desc` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' AFTER `name`;
@@ -37,7 +35,5 @@ ALTER TABLE `workflow_webot_trigger` ADD COLUMN `desc` varchar(1024) CHARACTER S
 ALTER TABLE `workflow_webot_trigger` ADD INDEX `idx_workflow_id`(`workflow_id` ASC) USING BTREE;
 
 ALTER TABLE `workflow_aggregate` ADD COLUMN `status` TINYINT(4) NOT NULL DEFAULT 0 COMMENT '状态（0:正常, -1:已删除）' AFTER `latest_publish_version`;
-
-ALTER TABLE `workflow_run` add column `thread_id` varchar(64) not null default '' comment 'thread_id' ;
 
 SET FOREIGN_KEY_CHECKS=1;

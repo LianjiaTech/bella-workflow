17f369ea81ea48a9a7a2b45c2dde84bae0b5a66e
XinyuZhang <zhangxinyu132@ke.com>
2025-09-08 16:16:59 +0800

    feat: display triggerFrom and trigger_id on the log query page (#37)
    * feat: display triggerFrom and trigger_id on the log query page
    
    * refactor: replace workflowSchedulingId with triggerId in runlogs

--- api/src/main/java/com/ke/bella/workflow/service/impl/DbWorkflowRunLogService.java
+++ api/src/main/java/com/ke/bella/workflow/service/impl/DbWorkflowRunLogService.java
@@ -84,6 +84,7 @@ public class DbWorkflowRunLogService implements IWorkflowRunLogService {
                 .workflowRunId(wr.getWorkflowRunId())
                 .flashMode(wr.getFlashMode() != null ? wr.getFlashMode() : 0)
                 .triggerFrom(wr.getTriggerFrom())
+				.triggerId(wr.getTriggerId())
                 .threadId(wr.getThreadId())
                 .stateful(wr.getStateful() != null && wr.getStateful() == 1)
                 .status(wr.getStatus())

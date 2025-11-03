e3897672691aa813bf4ecf1deabc1b38ecaa7d79
duanpengtao <duanpengtao@ke.com>
2025-11-03 15:41:49 +0800

    WorkflowContext add metadata

--- api/src/main/java/com/ke/bella/workflow/api/callbacks/WorkflowRunNotifyCallback.java
+++ api/src/main/java/com/ke/bella/workflow/api/callbacks/WorkflowRunNotifyCallback.java
@@ -1,13 +1,13 @@
 package com.ke.bella.workflow.api.callbacks;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import com.ke.bella.workflow.TaskExecutor;
 import com.ke.bella.workflow.WorkflowCallbackAdaptor;
 import com.ke.bella.workflow.WorkflowContext;
 import com.ke.bella.workflow.service.WorkflowService;
 import com.ke.bella.workflow.utils.HttpUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class WorkflowRunNotifyCallback extends WorkflowCallbackAdaptor {
     Map<String, Object> data = new LinkedHashMap<>();
@@ -25,6 +25,7 @@ public class WorkflowRunNotifyCallback extends WorkflowCallbackAdaptor {
         synchronized(data) {
             responseWorkflowInfo(context, data);
             responseWorkflowOutputs(context, data);
+			responseWorkflowMetaData(context, data);
         }
         TaskExecutor.submit(this::notifyClient);
 
